90044e95b50825dae11fc1384e0ed4e7b6d21df2
Wasif Khan <waasif.khaan@gmail.com>
2018-05-13 19:24:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :anag 
+  def initialize(word)
+    @anag = word
+  end
+  def match(wordlist)
+    wordlist.select do |comp|
+      comp.split("").sort == @anag.split("").sort 
+      comp
+    end
+  end
+end
